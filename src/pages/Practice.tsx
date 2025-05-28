742a62ef09a8c1992f911593fb30fffc32ee3a92
FranckHeCai <senlouboi.egame@gmail.com>
2025-05-28 18:58:58 +0200

    practice functionality done

--- src/pages/Practice.tsx
+++ src/pages/Practice.tsx
@@ -1,19 +1,21 @@
 import InfoIcon from "@icons/InfoIcon";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useKanjiStore } from "store/store";
-import type { QuestionType } from "types";
+import type { CompleteKanji, KanjiLesson, QuestionType } from "types";
 const Practice = () => {
   const {kanjiLessons, setCurrentLesson, currentLesson } = useKanjiStore(state => state)
   const {kanji, lesson} = useParams()
   const [currentIndex, setCurrentIndex] = useState(0)
   const [currentAnswerIndex, setCurrentAnswerIndex] = useState<null | number>(null)
+  const [currentAnswer, setCurrentAnswer] = useState<null | CompleteKanji>(null)
   const [questionType , setQuestionType] = useState<QuestionType>("")
+  const [shuffledAnswers, setShuffledAnswers] = useState<CompleteKanji[]>([])
+  const [checked, setChecked] = useState(false)
+  const [isCorrect, setIsCorrect] = useState(false)
   const questionTypes:QuestionType[] = ["onyomi", "kunyomi" , "meaning"]
 
   useEffect(() => {
-    const questionIndex = Math.floor(Math.random() * 3)
-    setQuestionType(questionTypes[questionIndex])
     setCurrentIndex(0)
     const kanjiLesson = Number(lesson)
     if(kanjiLessons){
@@ -21,15 +23,70 @@ const Practice = () => {
     }
   },[lesson, kanjiLessons])
 
-  useEffect(() => {
-    console.log(questionType)
-  },[questionType])
+  useEffect(()=>{
+    if(currentLesson){
+      setShuffledAnswers(shuffleAnswers(currentLesson))
+    }else{
+      setShuffledAnswers([])
+    }
+    const questionIndex = Math.floor(Math.random() * 3)
+    setQuestionType(questionTypes[questionIndex])
+    setCurrentAnswerIndex(null)
+    setChecked(false)
+    setCurrentAnswer(null)
+    setIsCorrect(false)
+
+  },[currentLesson, currentIndex])
+
+  const shuffleAnswers = (array:CompleteKanji[]) =>{
+    return [...array].sort(()=> Math.random() - 0.5 )
+  }
 
   const selectKanji = (index:number) => {
     setCurrentAnswerIndex(index)
   }
 
-  const handleCheckAnswer = () =>{
+  const handleCheckAnswer = () => {
+    if (!currentLesson || !currentAnswer) return;
+    setQuestionType("")
+    const currentKanji = currentLesson[currentIndex];
+    const selectedKanji = currentAnswer;
+
+    let isCorrect = false;
+
+    switch (questionType) {
+      case "onyomi":
+        isCorrect = selectedKanji.readings_on.some(reading =>
+          currentKanji.readings_on.includes(reading)
+        );
+        break;
+      case "kunyomi":
+        isCorrect = selectedKanji.readings_kun.some(reading =>
+          currentKanji.readings_kun.includes(reading)
+        );
+        break;
+      case "meaning":
+        isCorrect = selectedKanji.meanings.some(meaning =>
+          currentKanji.meanings.includes(meaning)
+        );
+        break;
+      default:
+        break;
+    }
+  
+    setIsCorrect(isCorrect);
+    setChecked(true);
+  }
+
+  const handleNextKanji = () => {
+    setCurrentIndex((prev):number => {
+      if(!currentLesson || prev === currentLesson.length - 1) return prev
+      window.scrollTo({ top: 0, behavior: "smooth" })
+      return prev + 1
+    })
+  }
+
+  const handleFinishPractice = () =>{
 
   }
 
@@ -75,11 +132,24 @@ const Practice = () => {
 
       <p>Select the correct answer</p>
       <div className="w-full sm:w-fit grid sm:grid-cols-2 lg:grid-cols-4 gap-2">
-        {currentLesson &&
-          currentLesson.map((kanji, index) => {
+        {shuffledAnswers &&
+          shuffledAnswers.map((kanji, index) => {
 
             return(
-              <button key={kanji.kanji} onClick={()=>{selectKanji(index)}} className={`${index === currentAnswerIndex ? " bg-slate-800 text-white" : "bg-white"} p-5  rounded-lg shadow-md`}>
+              <button key={kanji.kanji} onClick={()=>{
+                selectKanji(index)
+                setCurrentAnswer(kanji)
+                }} className={
+                  `${checked && currentAnswerIndex === index
+                      ? isCorrect
+                        ? "bg-emerald-500 text-white" 
+                        : "bg-red-500 text-white"
+                      : index === currentAnswerIndex 
+                      ? "bg-slate-800 text-white" 
+                      : "bg-white"
+                    } 
+                    p-5  rounded-lg shadow-md`
+                  }>
                   <p className="text-base sm:text-lg ">
                     { questionType === "kunyomi"
                         ? kanji.readings_kun.length === 0
@@ -92,11 +162,23 @@ const Practice = () => {
                   </p>
                 </button>
             )
-          })
+          }
+        )
         }
       </div>
-      <button disabled={currentAnswerIndex === null} onClick={handleCheckAnswer} className="px-4 py-2 bg-slate-800 text-white rounded disabled:opacity-50">
-        Confirm
+      <button disabled={currentAnswerIndex === null} onClick={()=>{
+        checked && currentLesson
+        ? currentIndex === currentLesson?.length - 1
+          ? handleFinishPractice()
+          : handleNextKanji()
+        : handleCheckAnswer()
+      }} className="px-4 py-2 bg-slate-800 text-white rounded disabled:opacity-50">
+        { checked && currentLesson
+            ? currentIndex === currentLesson?.length - 1
+              ? "End practice"
+              : "Next Kanji"
+            : "Confirm"
+        }
       </button>
     </div>
   );
